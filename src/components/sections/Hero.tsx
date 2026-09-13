5adc936768b3730a77f315e8cf6285a5eadf20f1
Toviho Segun <davidsegun044@gmail.com>
2026-09-13 14:48:19 +0100

    Fix formatting of text in Hero component

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -146,7 +146,7 @@ function Dial() {
         </defs>
         <text className="dial__text">
           <textPath href="#dial-path" startOffset="0">
-            SOFTWARE DEV · FULL-STACK · AI · WEB APP · SOLUTION ·
+            SOFTWARE DEV · FULL-STACK · AI · WEB APP · SOLUTION · 
           </textPath>
         </text>
       </motion.svg>
